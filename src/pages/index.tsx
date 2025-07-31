aff2fd4a86db1920a8ee0c901666841ef9da4c50
Sarah Kirchner <sarah.kirchner@sulzer.de>
2025-07-31 10:31:28 +0200

    Finished Gatsby Tutorial Part 3

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,17 @@
-import * as React from 'react';
-import Layout from '../components/layout';
+import * as React from "react";
+import { StaticImage } from "gatsby-plugin-image";
+import Layout from "../components/layout";
 
 const IndexPage = () => {
-    return (
-        <Layout pageTitle="Home Page">
-            <p>Welcome to the Home Page!</p>
-        </Layout>
-    )
+  return (
+    <Layout pageTitle="Home Page">
+      <p>Welcome to the Home Page!</p>
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="../images/clifford.webp"
+      />
+    </Layout>
+  );
 };
 
 export const Head = () => <title>Home Page</title>;
