a49ba0d4be389cc7eac94f0ca7ee53feaa43975f
Sarah Kirchner <sarah.kirchner@sulzer.de>
2025-08-01 10:17:21 +0200

    Finished Gatsby Tutorial Part 7

--- src/pages/blog/{mdx.frontmatter__slug}.tsx
+++ src/pages/blog/{mdx.frontmatter__slug}.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { graphql } from 'gatsby';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 import Layout from '../../components/layout';
 import Seo from '../../components/seo';
 
@@ -12,6 +13,14 @@ interface BlogPostProps {
         author: string;
         tags: string[];
         description: string;
+        hero_image: {
+          childImageSharp: {
+            gatsbyImageData: any;
+          };
+        };
+        hero_image_alt: string;
+        hero_image_credit_text: string;
+        hero_image_credit_link: string;
       };
     }
   },
@@ -19,13 +28,20 @@ interface BlogPostProps {
 }
 
 const BlogPost: React.FC<BlogPostProps> = ({data, children}) => {
-  console.log('BlogPost props:', data);
+  const heroImage = getImage(data.mdx.frontmatter.hero_image);
   return (
     <Layout pageTitle={data.mdx.frontmatter.title}>
       <p>{data.mdx.frontmatter.date}</p>
       <p><strong>Author:</strong> {data.mdx.frontmatter.author}</p>
       <p><strong>Tags:</strong> {data.mdx.frontmatter.tags.join(', ')}</p>
       <p>{data.mdx.frontmatter.description}</p>
+      <GatsbyImage image={heroImage!} alt={data.mdx.frontmatter.hero_image_alt} />
+      <p>
+        Photo Credit:{" "}
+        <a href={data.mdx.frontmatter.hero_image_credit_link} target={'_blank'}>
+          {data.mdx.frontmatter.hero_image_credit_text}
+        </a>
+      </p>
       {children}
     </Layout>
   )
@@ -40,6 +56,14 @@ export const query = graphql`
         author
         tags
         description
+        hero_image_alt
+        hero_image_credit_text
+        hero_image_credit_link
+        hero_image {
+          childImageSharp {
+            gatsbyImageData(layout: CONSTRAINED, width: 800, height: 400)
+          }
+        }
       }
     }
   }
